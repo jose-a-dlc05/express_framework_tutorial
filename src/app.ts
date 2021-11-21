08bc34143f0bbf26bf69b597c05f3b9be641166f
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-11-21 01:20:32 -0500

    Added the instructions for post_request portion

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-// 1. Import express and path module
+// 1. Import express and path module and body parser module
+
 // 2. Declare const app and initialize with express function returing object
 // 3. Use app middleware to give folder alias and using the static method to give the directory using join method
-// 4. Use the get method with a / path to send the html static along with its css and js
+
+// 4. Use app middleware to bring bodyParser
+// 5. Use the get method with a / path to send the html static along with its css and js
+
+// 6. Use the post method to log out the entered information from the form
+
 // 5. Use the listen method to begin running server
